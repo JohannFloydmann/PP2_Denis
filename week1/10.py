400ee18def8968b31a7827196e92f112cee33246
Denis <samrikov.denis@gmail.com>
2025-01-16 22:34:16 +0500

    string

---
+++ week1/10.py
@@ -0,0 +1,9 @@
+
+a = "Hello, World!"
+print(a[-5:-2])
+
+b = "Hello, World!"
+print(b[:5])
+
+c = "Hello, World!"
+print(c[2:])
---
+++ week1/9.py
@@ -0,0 +1,5 @@
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt2 = "The best things in life are free!"
+print("expensive" not in txt2)
