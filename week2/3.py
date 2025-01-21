bda7a7e38273626b7b1ce870604eaafb9fb936db
JohannFd <samrikov.denis@gmail.com>
2025-01-21 19:47:12 +0500

    start week2

---
+++ week2/3.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+thislist2 = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist2)
