e47b2db3eead328152dfc2b1080383d40b810145
Denis <samrikov.denis@gmail.com>
2025-01-31 16:44:46 +0500

    string

---
+++ week3/classes/1.py
@@ -0,0 +1,12 @@
+class String:
+    
+    def getString():
+        return input("Enter the string")
+    
+    def printString(st):
+        print(st)
+        
+string = String()
+
+a = string.getString()
+string.printString(a)
