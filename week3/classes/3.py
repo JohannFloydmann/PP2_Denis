04599a75b8a0b27b1867de61775de063e2827587
JohannFd <samrikov.denis@gmail.com>
2025-02-01 16:10:29 +0500

    rectangle

---
+++ week3/classes/3.py
@@ -0,0 +1,12 @@
+from shape import Shape
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        
+    def area(self):
+        return self.length * self.width
+    
+rect = Rectangle(2, 5)
+print(rect.area())
